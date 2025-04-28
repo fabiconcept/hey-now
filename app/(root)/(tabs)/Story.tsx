d6899a95e7bf1340eadcda6cfa39450def4d8f1c
fabiconcept <favourajokubi@gmail.com>
2025-04-28 04:24:16 +0100

    Create navigation pages

---
+++ app/(root)/(tabs)/Story.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Story = () => {
+    return (
+        <SafeAreaView>
+            <Text>Story</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Story
